95caec3a5212a7350f92edb58c385156220f68fb
Albert Schapiro <albert.schapiro@getdreams.com>
2017-11-07 21:32:57 +0100

    minor fixes to make it work twice..

--- RCTSFSafariViewController.m
+++ RCTSFSafariViewController.m
@@ -14,28 +14,28 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(openURL: (NSString *)urlString params:(NSDictionary *)params) {
   NSURL *url = [[NSURL alloc] initWithString: urlString];
 
-    dispatch_async(dispatch_get_main_queue(), ^{
-      UIViewController *rootViewController = [[[UIApplication sharedApplication] delegate] window].rootViewController;
-      while(rootViewController.presentedViewController) {
-        rootViewController = rootViewController.presentedViewController;
-      }
+  UIViewController *rootViewController = [[[UIApplication sharedApplication] delegate] window].rootViewController;
+  while(rootViewController.presentedViewController) {
+    rootViewController = rootViewController.presentedViewController;
+  }
 
-      SFSafariViewController *safariViewController = [[SFSafariViewController alloc] initWithURL:url];
-      UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:safariViewController];
+  SFSafariViewController *safariViewController = [[SFSafariViewController alloc] initWithURL:url];
+  UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:safariViewController];
 
-      [navigationController setNavigationBarHidden:YES animated:NO];
-      safariViewController.delegate = self;
+  [navigationController setNavigationBarHidden:YES animated:NO];
+  safariViewController.delegate = self;
 
-      if ([params objectForKey:@"tintColor"]) {
-        UIColor *tintColor = [RCTConvert UIColor:params[@"tintColor"]];
+  if ([params objectForKey:@"tintColor"]) {
+    UIColor *tintColor = [RCTConvert UIColor:params[@"tintColor"]];
 
-        if([safariViewController respondsToSelector:@selector(setPreferredControlTintColor:)]) {
-          safariViewController.preferredControlTintColor = tintColor;
-        } else {
-          safariViewController.view.tintColor = tintColor;
-        }
-      }
+    if([safariViewController respondsToSelector:@selector(setPreferredControlTintColor:)]) {
+      safariViewController.preferredControlTintColor = tintColor;
+    } else {
+      safariViewController.view.tintColor = tintColor;
+    }
+  }
 
+  dispatch_async(dispatch_get_main_queue(), ^{
     [rootViewController presentViewController:navigationController animated:YES completion:^{
       [self.bridge.eventDispatcher sendDeviceEventWithName:@"SFSafariViewControllerDidLoad" body:nil];
     }];
